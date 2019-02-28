d519a958de226254a63aded790c1e7731dbe8640
ansara.hooks <ansara.hooks@gmail.com>
2019-02-28 19:23:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,6 @@ def line (katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    katz_deli.each_with_index do |name|
-      puts "The line is currently: #{name}"
-    end
+    katz_deli.each_index{The line is currently:}
   end
 end
