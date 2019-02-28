829dbd8ab7fdb72d2412e16ece95c4e0d4c4e53d
ansara.hooks <ansara.hooks@gmail.com>
2019-02-28 20:43:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def take_a_number (katz_deli, person)
   puts "Welcome, #{person}. You are number #{katz_deli.size} in line."
 end
 
-def now_serving (katz_deli)
+def now_serving (katz_deli, person)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
