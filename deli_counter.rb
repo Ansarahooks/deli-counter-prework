f91582c8a1069916fabde178b29a20554862ae61
ansara.hooks <ansara.hooks@gmail.com>
2019-02-28 20:39:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def take_a_number (katz_deli, person)
   puts "Welcome, #{person}. You are number #{katz_deli.size} in line."
 end
 
-def now_serving
+def now_serving (katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   end
